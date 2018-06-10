f0c3d4c14e1207a28942db60cb9b03819ef69809
jancuz <jancuz@yandex.ru>
2018-06-10 16:25:47 +0500

    Добавлено меню для работы с графами (создание)

--- ex8/Program.cs
+++ ex8/Program.cs
@@ -6,8 +6,55 @@ namespace ex8
 {
     class Program
     {
+        static void PrintMenu()
+        {
+            Console.WriteLine("Введите номер пункта, который хотите выполнить:");
+            Console.WriteLine("1. Ввести граф с клавиатуры");
+            Console.WriteLine("2. Создать граф с помощью ДСЧ");
+            Console.WriteLine("3. Выход");
+        }
+
+        static void Run()
+        {
+            int checkRun = 0, left = 1, right = 3;
+            do
+            {
+                PrintMenu();
+                checkRun = AskData.ReadIntNumber("Введите номер пункта, который хотите выполнить", left, right);
+                switch (checkRun)
+                {
+                    case 1: // ввод графа с клавиатуры
+                        {
+                            Graph.FormGraph(out int[,] graph);                              // формирование графа
+                            Graph.ShowGraph(graph);                                         // печать графа
+                            int countVert = graph.GetLength(0);                             // кол-во вершин в графе
+                            int startVert = AskData.ReadIntNumber("Введите номер вершины, от которой требуется найти эйлеров цикл:", 1, countVert) - 1;
+                            Stack<int> res = new Stack<int>();                              // эйлеров цикл
+                            Graph.EulerCycle(startVert, ref res, countVert, ref graph);     // поиск эйлерого цикла
+                            Graph.ShowEulerCycle(res);                                      // печать цикла
+                            break;
+                        }
+
+                    case 2: // формирование графа с помощью ДСЧ
+                        {
+                            Graph.FormRandomGraph(out int[,] graph);                        // формирование графа
+                            Graph.ShowGraph(graph);                                         // печать графа
+                            int countVert = graph.GetLength(0);                             // кол-во вершин в графе
+                            int startVert = AskData.ReadIntNumber("Введите номер вершины, от которой требуется найти эйлеров цикл:", 1, countVert) - 1;
+                            Stack<int> res = new Stack<int>();                              // эйлеров цикл
+                            Graph.EulerCycle(startVert, ref res, countVert, ref graph);     // поиск эйлерого цикла
+                            Graph.ShowEulerCycle(res);                                      // печать цикла
+                            break;
+                        }
+
+                }
+            } while (checkRun != right);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Поиск Эйлерова цикла в графе");
+            Run();
         }
     }
 }
